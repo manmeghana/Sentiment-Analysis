5721fd4aebd0e5365d37be05b34ae2ab9a723da1
manmeghana <105707946+manmeghana@users.noreply.github.com>
2023-09-08 15:59:14 +0530

    Update deployment_sentiment_analysis.py

--- deployment_sentiment_analysis.py
+++ deployment_sentiment_analysis.py
@@ -17,10 +17,10 @@ import string
 import nltk
 import spacy
 
-with open("/content/svm_model.pkl","rb") as file:
+with open("svm_model.pkl","rb") as file:
   model = pickle.load(file)
 
-with open("/content/tfidf_vectorizer.pkl","rb") as file:
+with open("tfidf_vectorizer.pkl","rb") as file:
   vectorizer = pickle.load(file)
 
 nltk.download('stopwords')
